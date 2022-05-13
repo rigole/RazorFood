11a1625dc1591e068ee9cb98748de7d00637ac22
rigole <foplacide@gmail.com>
2022-05-13 16:48:23 +0100

    setting the Food model 1

---
+++ RazorFood/Models/FoodModel.cs
@@ -0,0 +1,20 @@
+﻿ namespace RazorFood.Models
+{
+    public class FoodModel
+    {
+        public string ImageTitle { get; set; }
+
+        public string FoodName { get; set; }
+
+        public float BasePrice { get; set; }
+
+        public bool FoodSauce  { get; set; }
+
+        public bool Pepper { get; set; }
+
+        public bool Mushroom { get; set; }
+
+        public bool Tuna { get; set; }
+
+    }
+}
