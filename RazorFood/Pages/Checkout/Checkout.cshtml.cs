b537a60454c0ad35b0def76d016628248f84921c
rigole <foplacide@gmail.com>
2022-05-23 20:24:21 +0100

    Displaying data from URL with OnGet method

--- RazorFood/Pages/Checkout/Checkout.cshtml.cs
+++ RazorFood/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorFood.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string FoodName { get; set; }
+        public float FoodPrice { get; set; }
+        public string ImageTitle { get; set; }
+         
         public void OnGet()
         {
+
+            if (string.IsNullOrWhiteSpace(FoodName))
+            {
+                FoodName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
         }
     }
 }
