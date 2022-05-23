cdd5dbe6a6b1c8d2c435acc44775def408c24f16
rigole <foplacide@gmail.com>
2022-05-23 19:18:47 +0100

    setting IActionResult

--- RazorFood/Pages/Forms/CustomFood.cshtml.cs
+++ RazorFood/Pages/Forms/CustomFood.cshtml.cs
@@ -9,8 +9,25 @@ namespace RazorFood.Pages.Forms
         [BindProperty]
 
         public FoodsModel Food { get; set; }
+
+        public float FoodPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            FoodPrice = Food.BasePrice;
+
+            if (Food.FoodSauce) FoodPrice += 1;
+            if (Food.Mushroom) FoodPrice += 1;
+            if (Food.Tuna) FoodPrice += 1;
+            if (Food.Pineapple) FoodPrice += 10;
+            if (Food.Ham) FoodPrice += 1;
+            if (Food.Pepper) FoodPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new  { Food.FoodName, FoodPrice});
+
         }
     }
 }
