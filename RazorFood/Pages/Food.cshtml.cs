2eeeb6408ffa722dc0cfe521b0fbed71939d7f47
rigole <foplacide@gmail.com>
2022-05-29 10:33:00 +0100

    Rendering a list of food using static list

--- RazorFood/Pages/Food.cshtml.cs
+++ RazorFood/Pages/Food.cshtml.cs
@@ -5,8 +5,59 @@ namespace RazorFood.Pages
 {
     public class FoodModel : PageModel
     {
-       // [BindProperty]
-        //public FoodModel Food { get; set; }
+        // [BindProperty]
+        public List<FoodsModel> foodsModels = new List<FoodsModel>() 
+         { 
+            new FoodsModel() {
+                ImageTitle="Eru", 
+                FoodName="Eru", 
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+             new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+              new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+               new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+                new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+                 new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            },
+                  new FoodsModel() {
+                ImageTitle="Eru",
+                FoodName="Eru",
+                BasePrice = 1000,
+                Pepper=true,
+                FinalPrice = 4
+            }
+        };
         public void OnGet()
         {
         }
